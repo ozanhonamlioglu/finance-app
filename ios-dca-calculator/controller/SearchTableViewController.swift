5f1b6ed63313fec7b5d876f74f574e5dce07c88c
ozan honamlioglu <ozan.honamlioglu@inveon.com>
2021-05-25 23:41:17 +0300

    Calculator UI Design

--- ios-dca-calculator/controller/SearchTableViewController.swift
+++ ios-dca-calculator/controller/SearchTableViewController.swift
@@ -113,6 +113,10 @@ class SearchTableViewController: UITableViewController, UIAnimatable {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showCalculator", sender: nil)
+    }
 
 }
 
